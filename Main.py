574b4beb2fe1aa6d8b8ffcb49874696f635e4131
LeeM123321 <23mcclintockl@mountainview.school.nz>
2025-08-27 12:52:37 +1200

    updated 3rd incorrect text

--- Main.py
+++ Main.py
@@ -1,23 +1,4 @@
-"""What the program needs to do
-At the start of the game, get the users name and use their name throughout the game (where appropriate).
-The game is designed for children from 8 to 13 years of age.
-If the child is older (14 years plus) they should be encouraged to try the Cybersmart Youth Quiz instead. 
-Note: You do not need to create the Cybersmart Youth Quiz.
 
-Questions and answers
-The program should include at least three questions. 
-The questions and their correct answers should be stored in lists in your program.
-Examples of questions and answers for the quiz are on the next page. 
-
-Scoring
-If the child gets the answer correct the first time, they get one point for the question.
-If they get the answer wrong, they should be encouraged to try again – two more times 
-(ie a total of three tries). If they still can’t get it right, they are told the correct answer, 
-and move on to the next question.
-
-Important: They only get a point for a question if they get it correct the first time they try.
-At the end of the quiz the child should be given their score (ie the number of questions they got right on their first try).
-"""
 def quiz():
     
     
@@ -39,7 +20,7 @@ def quiz():
         quit()
     else:
         _ = input("You are in the recommended age category, press enter to start.")
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print("\n" * 20)
         
         #asking question 1
         for i in range(3):
@@ -50,21 +31,21 @@ def quiz():
             Guess = input("Please answer the question with A B or C: ").upper()
             if Guess == Q1I[4]:
                 if i == 0:
-                    _ = input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
                     correctAnswers += 1
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\n" * 20)
                     break
                 else:
-                    _ = input("Thats correct! press enter to continue.")
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    input("Thats correct! press enter to continue.")
+                    print("\n" * 20)
                     break
             else:
                     if i == 2:
-                        _ = input("Sorry that is incorrect, due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input(f"Sorry that is incorrect, the correct answer was {Q1I[4]} due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                        print("\n" * 20)
                     else:
-                        _ = input("Sorry that is incorrect, please press enter to try again")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input("Sorry that is incorrect, please press enter to try again")
+                        print("\n" * 20)
                         
         #asking question 2
         for i in range(3):
@@ -75,21 +56,21 @@ def quiz():
             Guess = input("Please answer the question with A B or C: ").upper()
             if Guess == Q2I[4]:
                 if i == 0:
-                    _ = input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
                     correctAnswers += 1
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\n" * 20)
                     break
                 else:
-                    _ = input("Thats correct! press enter to continue.")
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    input("Thats correct! press enter to continue.")
+                    print("\n" * 20)
                     break
             else:
                     if i == 2:
-                        _ = input("Sorry that is incorrect, due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input("Sorry that is incorrect, the correct answer was",Q2I[4],"due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                        print("\n" * 20)
                     else:
-                        _ = input("Sorry that is incorrect, please press enter to try again")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input("Sorry that is incorrect, please press enter to try again")
+                        print("\n" * 20)
                         
         #asking question 3
         for i in range(3):
@@ -101,21 +82,21 @@ def quiz():
             print()
             if Guess == Q3I[4]:
                 if i == 0:
-                    _ = input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
                     correctAnswers += 1
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\n" * 20)
                     break
                 else:
-                    _ = input("Thats correct! press enter to continue.")
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    input("Thats correct! press enter to continue.")
+                    print("\n" * 20)
                     break
             else:
                     if i == 2:
-                        _ = input("Sorry that is incorrect, due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input("Sorry that is incorrect, the correct answer was",Q3I[4],"due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                        print("\n" * 20)
                     else:
-                        _ = input("Sorry that is incorrect, please press enter to try again")
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        input("Sorry that is incorrect, please press enter to try again")
+                        print("\n" * 20)
                         
         print("you scored ", correctAnswers, " out of ", totalQuestions)
                         
