ed1e5663dcbb8b09b0b3dd9a09c5785bd0c21ea2
LeeM123321 <23mcclintockl@mountainview.school.nz>
2025-08-29 08:47:40 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ def quiz():
     
     correctAnswers = 0
     totalQuestions = 3
+    Options = ["A","B","C"]
     #Q1I = question one information
     # format = [question, option A, option B, option C, answer] 
     Q1I = ["Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do?","Delete the message and try to forget about it","Keep the text and show an adult you trust","Text the person back saying something mean to them","B"]
@@ -16,89 +17,108 @@ def quiz():
     #checking if under correct age
     age = int(input("Input Age: "))
     if age > 13:
-        print("You are older than the recommended age category, we recommend you try out our \"Cybersmart Youth Quiz\" instead.")
-        quit()
+        input("You are older than the recommended age category, we recommend you try out our \"Cybersmart Youth Quiz\" if you wish to continue press enter")
     else:
-        _ = input("You are in the recommended age category, press enter to start.")
-        print("\n" * 20)
-        
-        #asking question 1
-        for i in range(3):
-            print(Q1I[0])
-            print("A: ",Q1I[1])
-            print("B: ",Q1I[2])
-            print("C: ",Q1I[3])
+        input("You are in the recommended age category, press enter to start.")
+    print("\n" * 20)
+    
+    #asking question 1
+    for i in range(3):
+        #printing question 1
+        print(Q1I[0])
+        print("A: ",Q1I[1])
+        print("B: ",Q1I[2])
+        print("C: ",Q1I[3])
+        #getting users guess and checking if its A B or C
+        while Guess not in Options:
             Guess = input("Please answer the question with A B or C: ").upper()
-            if Guess == Q1I[4]:
-                if i == 0:
-                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
-                    correctAnswers += 1
-                    print("\n" * 20)
-                    break
-                else:
-                    input("Thats correct! press enter to continue.")
-                    print("\n" * 20)
-                    break
+        #resetting guess for next question
+        Guess = 0
+        #checking if guess is correct and checking if it was on the first try
+        if Guess == Q1I[4]:
+            if i == 0:
+                input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                correctAnswers += 1
+                print("\n" * 20)
+                break
             else:
-                    if i == 2:
-                        input(f"Sorry that is incorrect, the correct answer was {Q1I[4]} due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n" * 20)
-                    else:
-                        input("Sorry that is incorrect, please press enter to try again")
-                        print("\n" * 20)
-                        
-        #asking question 2
-        for i in range(3):
-            print(Q2I[0])
-            print("A: ",Q2I[1])
-            print("B: ",Q2I[2])
-            print("C: ",Q2I[3])
+                input("Thats correct! press enter to continue.")
+                print("\n" * 20)
+                break
+            #checking if this is the 3rd time getting it wrong
+        else:
+                if i == 2:
+                    input(f"Sorry that is incorrect, the correct answer was {Q1I[4]} and due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                    print("\n" * 20)
+                else:
+                    input("Sorry that is incorrect, please press enter to try again")
+                    print("\n" * 20)
+                    
+    #asking question 2
+    for i in range(3):
+        #printing question 2
+        print(Q2I[0])
+        print("A: ",Q2I[1])
+        print("B: ",Q2I[2])
+        print("C: ",Q2I[3])
+        #getting users guess and checking if its A B or C
+        while Guess not in Options:
             Guess = input("Please answer the question with A B or C: ").upper()
-            if Guess == Q2I[4]:
-                if i == 0:
-                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
-                    correctAnswers += 1
-                    print("\n" * 20)
-                    break
-                else:
-                    input("Thats correct! press enter to continue.")
-                    print("\n" * 20)
-                    break
+        #resetting guess for next question
+        Guess = 0
+        #checking if guess is correct and checking if it was on the first try
+        if Guess == Q2I[4]:
+            if i == 0:
+                input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                correctAnswers += 1
+                print("\n" * 20)
+                break
             else:
-                    if i == 2:
-                        input("Sorry that is incorrect, the correct answer was",Q2I[4],"due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n" * 20)
-                    else:
-                        input("Sorry that is incorrect, please press enter to try again")
-                        print("\n" * 20)
-                        
-        #asking question 3
-        for i in range(3):
-            print(Q3I[0])
-            print("A: ",Q3I[1])
-            print("B: ",Q3I[2])
-            print("C: ",Q3I[3])
+                input("Thats correct! press enter to continue.")
+                print("\n" * 20)
+                break
+            #checking if this is the 3rd time getting it wrong
+        else:
+                if i == 2:
+                    input(f"Sorry that is incorrect, the correct answer was {Q2I[4]} and due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                    print("\n" * 20)
+                else:
+                    input("Sorry that is incorrect, please press enter to try again")
+                    print("\n" * 20)
+                    
+    #asking question 3
+    for i in range(3):
+        #printing question 3
+        print(Q3I[0])
+        print("A: ",Q3I[1])
+        print("B: ",Q3I[2])
+        print("C: ",Q3I[3])
+        #getting users guess and checking if its A B or C
+        while Guess not in Options:
             Guess = input("Please answer the question with A B or C: ").upper()
-            print()
-            if Guess == Q3I[4]:
-                if i == 0:
-                    input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
-                    correctAnswers += 1
-                    print("\n" * 20)
-                    break
-                else:
-                    input("Thats correct! press enter to continue.")
-                    print("\n" * 20)
-                    break
+        #resetting guess for next question
+        Guess = 0
+        #checking if guess is correct and checking if it was on the first try
+        if Guess == Q3I[4]:
+            if i == 0:
+                input("Thats correct! you have gained 1 point for getting it correct on your first try, press enter to continue.")
+                correctAnswers += 1
+                print("\n" * 20)
+                break
             else:
-                    if i == 2:
-                        input("Sorry that is incorrect, the correct answer was",Q3I[4],"due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
-                        print("\n" * 20)
-                    else:
-                        input("Sorry that is incorrect, please press enter to try again")
-                        print("\n" * 20)
-                        
-        print("you scored ", correctAnswers, " out of ", totalQuestions)
-                        
+                input("Thats correct! press enter to continue.")
+                print("\n" * 20)
+                break
+            #checking if this is the 3rd time getting it wrong
+        else:
+                if i == 2:
+                    input(f"Sorry that is incorrect, the correct answer was {Q3I[4]} and due to getting the question wrong three times in a row it will be skipped, please press enter to continue")
+                    print("\n" * 20)
+                else:
+                    input("Sorry that is incorrect, please press enter to try again")
+                    print("\n" * 20)
+    #printing score
+    print("you scored ", correctAnswers, " out of ", totalQuestions)
+                    
 
 quiz()
